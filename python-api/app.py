8138f808aa3016b5daea71da198707c671646351
Leith Zniber <leith311.z@hotmail.fr>
2026-05-21 10:30:55 +0200

    reparation des 6 bugs concernant pythons

--- python-api/app.py
+++ python-api/app.py
@@ -9,13 +9,15 @@ config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'co
 with open(config_path, 'r') as f:
     config = json.load(f)
 
+log_file = "./server.log"
+
 # -------------------------------------------------------
 # Analyse un fichier de logs serveur et retourne
 # le nombre d'erreurs, warnings et infos détectés.
 # -------------------------------------------------------
 
 # BUG 2 — Il manque un caractère essentiel à la fin de cette ligne
-def parse_logs(filepath)
+def parse_logs(filepath):
     erreurs = []
     warnings = []
     infos = []
@@ -28,16 +30,16 @@ def parse_logs(filepath)
             # BUG 3 — Le nom de la variable utilisée ici ne correspond pas
             #         à celle déclarée plus haut dans cette fonction
             if "ERROR" in line:
-                errors.append(line)
+                    erreurs = [].append(line)
             elif "WARNING" in line:
                 warnings.append(line)
             elif "INFO" in line:
                 infos.append(line)
 
     return {
-        "error_count": len(erreurs),
-        "warning_count": len(warnings),
-        "info_count": len(infos),
+        "error_count": 0 if not erreurs else len(erreurs),
+        "warning_count": 0 if not warnings else len(warnings),
+        "info_count": 0 if not infos else len(infos),
         "errors": erreurs,
         "warnings": warnings
     }
