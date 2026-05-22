c88cc9b16345ef1141a5b3e88c56848c4ea66e37
malikcherfi <malikcherfi@gmail.com>
2026-05-22 10:30:47 +0200

    run on host 0.0.0.0

--- python-api/app.py
+++ python-api/app.py
@@ -58,4 +58,4 @@ def get_logs():
 if __name__ == "__main__":
     # Le port est chargé depuis config.json
     # BUG 5 — Le port est défini dans config.json — est-il correct ?
-    app.run(debug=True, port=config["api"]["port"])
+    app.run(host="0.0.0.0", debug=True, port=config["api"]["port"])
